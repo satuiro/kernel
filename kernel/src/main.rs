588b8f09e129beae557bd43adcb4b46d127f5066
satuiro <mohakmalhotra008@gmail.com>
2024-06-02 00:49:26 +0530

    Displaying text and shapes using logger implementation

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -1,7 +1,22 @@
 #![no_std]
 #![no_main]
 
+mod framebuffer;
+
 use core::panic::PanicInfo;
+use bootloader_api::info::FrameBufferInfo;
+use bootloader_x86_64_common::logger::LockedLogger;
+use conquer_once::spin::OnceCell;
+
+
+pub(crate) static LOGGER: OnceCell<LockedLogger> = OnceCell::uninit();
+
+pub(crate) fn init_logger(buffer: &'static mut [u8], info: FrameBufferInfo) {
+    let logger = LOGGER.get_or_init(move || LockedLogger::new(buffer, info, true, false));
+    log::set_logger(logger).expect("Logger alreasy set");
+    log::set_max_level(log::LevelFilter::Trace);
+    log::info!("The mathematics says that 2 + 2 = {}", 2 + 2);
+}
 
 /// This function is called on panic.
 #[panic_handler]
@@ -11,11 +26,35 @@ fn panic(_info: &PanicInfo) -> ! {
 
 bootloader_api::entry_point!(kernel_main);
 
-fn kernel_main(_boot_info: &'static mut bootloader_api::BootInfo) -> ! {
-    if let Some(framebuffer) = _boot_info.framebuffer.as_mut() {
-        for byte in framebuffer.buffer_mut() {
-            *byte = 0x90;
+fn kernel_main(boot_info: &'static mut bootloader_api::BootInfo) -> ! {
+    // Drawing a simple RGB pixel over the kernel
+    if let Some(framebuffer) = boot_info.framebuffer.as_mut() {
+        let color = framebuffer::Color {
+            red: 23,
+            green: 162,
+            blue: 255,
+        };
+        for x in 0..100 {
+            for y in 0..100 {
+                let position = framebuffer::Position {
+                    x: 20 + x,
+                    y: 100 + y,
+                };
+                framebuffer::set_pixel_in(framebuffer, position, color);
+            }
         }
     }
+
+    let frame_buffer_optional = &mut boot_info.framebuffer;
+    let frame_buffer_option = frame_buffer_optional.as_mut();
+    let frame_buffer_struct = frame_buffer_option.unwrap();
+    let frame_buffer_info = frame_buffer_struct.info().clone();
+
+    // get the framebuffer's mutable raw byte slice
+    let raw_frame_buffer = frame_buffer_struct.buffer_mut();
+
+    // finally, initialize the logger using the last two variables
+    init_logger(raw_frame_buffer, frame_buffer_info);
+
     loop {}
 }
